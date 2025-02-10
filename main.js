5dff031814c9164c39ce380eb455414fc799e477
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2025-02-10 10:57:25 -0800

    Support pagination
    … and also support a retry if we hit Notion's rate limit

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { Client } = require('@notionhq/client');
+const core = require('@actions/core');
 const { NotionToMarkdown } = require('notion-to-md');
+const { backOff } = require('exponential-backoff');
 const fs = require('fs').promises;
 
 const notionClient = new Client({ auth: process.env['INPUT_NOTION-TOKEN'] });
@@ -7,17 +9,39 @@ const notionClient = new Client({ auth: process.env['INPUT_NOTION-TOKEN'] });
 const notionToMarkdown = new NotionToMarkdown({ notionClient });
 
 (async () => {
-  const search = await notionClient.search({});
+  let arguments = { };
 
-  search.results.forEach (async result => {
-    const baseName = result.url.split('/').pop();
+  let search = { };
 
-    const mdBlocks = await notionToMarkdown.pageToMarkdown(result.id);
+  do {
+    search = await notionClient.search(arguments);
 
-    const { parent } = notionToMarkdown.toMarkdownString(mdBlocks);
+    for (const result of search.results) {
+      if (!("url" in result)) {
+        core.warning(`Result missing 'url' field: ${JSON.stringify(result)}`)
 
-    if(typeof parent !== "undefined") {
-      await fs.writeFile(`${baseName}.md`, parent);
+        continue;
+      }
+
+      const baseName = result.url.split('/').pop();
+
+      const backoffOptions = {
+        startingDelay: 60 * 60 * 1000,
+        numOfAttempts: 2,
+        timeMultiple: 1,
+      };
+
+      const mdBlocks = await backOff(() => notionToMarkdown.pageToMarkdown(result.id), backoffOptions);
+
+      const { parent } = notionToMarkdown.toMarkdownString(mdBlocks);
+
+      if(typeof parent !== "undefined") {
+        await fs.writeFile(`${baseName}.md`, parent);
+      }
     }
-  });
+
+    if (search.has_more) {
+      arguments = { start_cursor: search.next_cursor };
+    }
+  } while (search.has_more);
 })();
